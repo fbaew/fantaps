ef09574bfdbcd2f29bcef084052bd30b108e2c81
Gregg Lewis <code@gregglewis.net>
2015-08-26 01:53:47 -0600

    Enable some logging...

--- crawler/articles/management/commands/get_news.py
+++ crawler/articles/management/commands/get_news.py
@@ -1,19 +1,23 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-from articles.models import Article
+from articles.models import Article, Feed
 
 class Command(BaseCommand):
     args = ""
     help = "scrape stuff and make articles from it..."
 
-    def _populate(self):
+#    def _populate(self):
+#
+#        for i in range(10):
+#            a = Article()
+#            a.article_url = "http://gregglewis.net/test/{}".format(i)
+#            a.article_title = "Gregg's Article #{}".format(i)
+#            a.pub_date = timezone.now()
+#            a.save()
 
-        for i in range(10):
-            a = Article()
-            a.article_url = "http://gregglewis.net/test/{}".format(i)
-            a.article_title = "Gregg's Article #{}".format(i)
-            a.pub_date = timezone.now()
-            a.save()
+    def _populate(self):
+        for feed in Feed.objects.all():
+            feed.get_latest_articles()
 
     def handle(self, *args, **options):
         self._populate()
--- crawler/articles/models.py
+++ crawler/articles/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.utils import timezone
+import logging
+
 
 # Create your models here.
 
 def get_default_feed():
+    '''
+
+    Dummy method to return our bogus feed... 
+    probably can do better than just returning 3.
+
+    '''
     return 3#    return Feed.objects.get(pk=3)
 
 
@@ -11,9 +19,10 @@ def get_default_feed():
 class Feed(models.Model):
     last_ripped = models.DateTimeField(null=True)
     feed_url = models.TextField(max_length=500)
+    log = logging.getLogger(__name__)
 
     def get_latest_articles(self):
-        pass
+        self.log.debug("Getting latest articles for feed {}".format(self.feed_url))
 
 
 class Article(models.Model):
