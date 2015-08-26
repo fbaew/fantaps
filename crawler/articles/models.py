0eb8a0ff5ef85af82208e93f0964e17502fa82c3
Gregg Lewis <code@gregglewis.net>
2015-08-25 23:06:14 -0600

    yeee feed foreign key bitches

--- crawler/articles/models.py
+++ crawler/articles/models.py
@@ -3,12 +3,24 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Article(models.Model):
-    pub_date = models.DateTimeField()
-    article_url = models.TextField(max_length=500)
-    article_title = models.TextField(max_length=200)
+def get_default_feed():
+    return 3#    return Feed.objects.get(pk=3)
+
+
 
 class Feed(models.Model):
     last_ripped = models.DateTimeField(null=True)
     feed_url = models.TextField(max_length=500)
+
+    def get_latest_articles(self):
+        pass
+
+
+class Article(models.Model):
+    
+    pub_date = models.DateTimeField()
+    article_url = models.TextField(max_length=500)
+    article_title = models.TextField(max_length=200)
+    parent_feed = models.ForeignKey("Feed", default=get_default_feed)
+
 
