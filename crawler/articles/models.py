035dc1c10dfee4b43e1165a127e131ccdb28ca8e
Gregg Lewis <code@gregglewis.net>
2015-08-25 22:37:21 -0600

    store sources as Feed objects

--- crawler/articles/models.py
+++ crawler/articles/models.py
@@ -8,3 +8,7 @@ class Article(models.Model):
     article_url = models.TextField(max_length=500)
     article_title = models.TextField(max_length=200)
 
+class Feed(models.Model):
+    last_ripped = models.DateTimeField(null=True)
+    feed_url = models.TextField(max_length=500)
+
