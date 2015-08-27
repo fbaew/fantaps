ddd102930a85f336311f1dfcd90c4ca231a5b9c2
Gregg Lewis <code@gregglewis.net>
2015-08-26 22:59:14 -0600

    Add some tests

--- crawler/articles/models.py
+++ crawler/articles/models.py
@@ -26,10 +26,10 @@ class Feed(models.Model):
 
 
 class Article(models.Model):
-    
     pub_date = models.DateTimeField()
     article_url = models.TextField(max_length=500)
     article_title = models.TextField(max_length=200)
+    article_text = models.TextField(max_length=20000)
     parent_feed = models.ForeignKey("Feed", default=get_default_feed)
 
 
